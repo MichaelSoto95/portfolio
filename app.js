851582eecc8222b3f8c915d8e66285baf3ee31b9
Michael Soto <msoto@ayacol.com>
2023-02-02 16:08:39 -0400

    3rd commit

--- app.js
+++ app.js
@@ -11,7 +11,19 @@
 
     })
 
+    // let lastScrollTop = 0;
 
+    // // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+    // element.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+    //    let st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    //    if (st > lastScrollTop) {
+    //   alert("top");
+    //    } else if (st < lastScrollTop) {
+    //       // upscroll code
+    //       alert("bottom");
+    //    } // else was horizontal scroll
+    //    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    // }, false);
 //     const links = document.querySelectorAll(" .ul .a");
 // for (const link of links) {
 //   link.addEventListener("click", clickHandler);
